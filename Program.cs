f14e93e6b36f0e41f638a1b22f8f1407f60228db
etniesz <jaime-.g@hotmail.cl>
2023-10-17 13:24:17 -0300

    uploading project

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿List<object> objectsArray = new List<object>();
+objectsArray.Add(7);
+objectsArray.Add(28);
+objectsArray.Add(-1);
+objectsArray.Add(true);
+objectsArray.Add("chair");
+
+foreach (object obj in objectsArray)
+{
+    Console.WriteLine(obj);
+}
+
+int temp = 0;
+for (int i = 0; i < objectsArray.Count; i++)
+{
+    if (objectsArray[i] is int)
+    {
+        temp = temp + (int)objectsArray[i];
+    }
+
+}
+
+Console.WriteLine($"La suma total es: {temp}");
